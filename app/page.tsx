97110a3b4c4e47f9b44c8234f183acaf9aa53b1e
rzxldz <thebrock2004@gmail.com>
2026-09-08 23:13:06 -0600

    Add LinkedIn profile

--- app/page.tsx
+++ app/page.tsx
@@ -319,10 +319,14 @@ export default function Home() {
                   <Arrow />
                 </a>
 
-                <a href="#">
-                  LinkedIn
-                  <Arrow />
-                </a>
+                <a
+  href="https://www.linkedin.com/in/jorge-emmanuel-roldán-márquez-499a13434"
+  target="_blank"
+  rel="noreferrer"
+>
+  LinkedIn
+  <Arrow />
+</a>
               </div>
             </div>
           </div>
